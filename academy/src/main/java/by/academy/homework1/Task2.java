94ae164c8e3e73c6910f4a1a935fa7ee04bc2452
Uladzislau Splendzer <splendzer@gmail.com>
2021-11-04 18:31:07 +0300

    commit message

---
+++ academy/src/main/java/by/academy/homework1/Task2.java
@@ -0,0 +1,10 @@
+package by.academy.homework1;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// на больничном, не успел сделать
+
+	}
+
+}
---
+++ academy/src/main/java/by/academy/homework1/Task3.java
@@ -0,0 +1,10 @@
+package by.academy.homework1;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		//// на больничном, не успел сделать
+
+	}
+
+}
