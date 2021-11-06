0bd78abb2e4f029a2972d8e1837d5dfd768705be
Uladzislau Splendzer <splendzer@gmail.com>
2021-11-06 21:31:12 +0300

    06.11

--- academy/src/main/java/by/academy/homework1/Task2.java
+++ academy/src/main/java/by/academy/homework1/Task2.java
@@ -1,10 +1,63 @@
 package by.academy.homework1;
 
+import java.util.Scanner;
+
+
 public class Task2 {
-
-	public static void main(String[] args) {
-		// на больничном, не успел сделать
-
-	}
-
-}
+    public static void main(String[] args) {
+        System.out.print("Введите тип данных: ");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        switch (str) {
+           
+        		case "int":
+                System.out.print("Введите значение переменной типа int: ");
+                if(sc.hasNextInt()) {
+                    int var1 = sc.nextInt();
+                    int result1 = var1 % 2;
+                    System.out.println("Остаток от деления на 2 переменной " + var1 + " равен " + result1);
+                } else {
+                    System.out.println("Incorrect type value. Int type value is required");
+                }
+                break;
+                
+        		case "double":
+                System.out.print("Введите значение переменной типа double: ");
+                if(sc.hasNextDouble()) {
+                    double var2 = sc.nextDouble();
+                    double result2 = var2 * 0.7;
+                    System.out.println("70% от числа " + var2 + " равно " + result2);
+                } else{
+                    System.out.println("Incorrect type value. Double type value is required");
+                }
+                break;
+                
+        		case "float":
+                System.out.print("Введите значение переменной типа float: ");
+                if (sc.hasNextFloat()) {
+                  float var3 = sc.nextFloat();
+                  double result3 = var3 * var3;
+                  System.out.println(var3 + " в квадрате равно " + result3);
+                } else {
+                  System.out.println("Incorrect type value. Float type value is required");
+                }
+                break;
+                
+        		case "String":
+                System.out.print("Введите значение переменной типа String: ");
+                    String var4 = sc.nextLine();
+                    System.out.println("Hello " + var4);
+                break;
+                
+        		case "char":
+                System.out.print("Введите значение переменной: ");
+                String var5 = sc.nextLine();
+                System.out.println("Код символа " + var5.charAt(0) + " соответствует коду " + (int)(var5.charAt(0)));
+                break;
+                
+        		default:
+                System.out.println("Unsupported type");
+                break;
+        }
+        }
+    }
--- academy/src/main/java/by/academy/homework1/Task3.java
+++ academy/src/main/java/by/academy/homework1/Task3.java
@@ -1,10 +1,18 @@
 package by.academy.homework1;
 
+import java.util.Scanner;
+
 public class Task3 {
-
-	public static void main(String[] args) {
-		//// на больничном, не успел сделать
-
-	}
-
-}
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Введите число от 1 до 10: ");
+        int n = s.nextInt();
+        int result;
+        System.out.println("Таблица умножения для числа: " + n);
+        for (int i = 1; i <= 10; i++) {
+        result = n * i;
+        System.out.println(n + "*" + i + " = " + result);
+            
+        }
+    }
+}6666666
