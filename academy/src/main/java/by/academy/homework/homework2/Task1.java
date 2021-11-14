120b1127866226c9b1f30a25ddd40a0262005a4a
Uladzislau Splendzer <splendzer@gmail.com>
2021-11-14 18:43:37 +0300

    v2

--- academy/src/main/java/by/academy/homework/homework2/Task1.java
+++ academy/src/main/java/by/academy/homework/homework2/Task1.java
@@ -13,28 +13,30 @@ public class Task1 {
 		int count1 = 0;
 		int count2 = 0;
 		
-		
-		if (str1.length() == str2.length()) {
-			for (int i = 0; i < str1.length(); i++) {
-				char check = str1.charAt(i);
-				for (int j = 0; j < str1.length(); j++) {
-					if (str1.charAt(j) == check) {
-						count1++;
+		if ((str1 != null) && (str2 != null)) {
+			if (str1.length() == str2.length()) {
+				for (int i = 0; i < str1.length(); i++) {
+					char check = str1.charAt(i);
+					for (int j = 0; j < str1.length(); j++) {
+						if (str1.charAt(j) == check) {
+							count1++;
 						}
-							if (str2.charAt(j) == check) {
-								count2++;
-								}
-		}
-		if (count1 != count2) {
-		System.out.println("Вторая строка не является перестановкой символов первой строки.");
-		return;
-		}
-		}
-		System.out.println("Вторая строка является перестановкой символов первой строки.");
+						if (str2.charAt(j) == check) {
+							count2++;
+						}
+					}
+					if (count1 != count2) {
+						System.out.println("Разная длина строк");
+						return;
+					}
+				}
+				System.out.println("Вторая строка является перестановкой символов первой строки");
+			} else {
+				System.out.println("Вторая строка не является перестановкой символов первой строки");
+			}
 		} else {
-				System.out.println("Разная длина у введенных строк.");
-			} 
+			System.out.println("Строка или строки null!");
 			sc.close();
-		
+		}
 	}
 }
